7ae1bce1a7f232d0d1c2bb68621962a6fddc3b9b
Rajesh Varikuntla <raj333921@gmail.com>
2020-03-05 18:17:59 +0100

    Update runLocal.sh

--- runLocal.sh
+++ runLocal.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 mvn clean install
+
 sudo docker build -t employee-web:1.0 .
 
 sudo docker run --name=mysql1 -d mysql/mysql-server:8.0
-sudo docker run -d -p 8080:8080 -t employee-web:1.0
+
+sudo docker run -d --name=employee-web -p 8080:8080 -t employee-web:1.0
+
+
 
