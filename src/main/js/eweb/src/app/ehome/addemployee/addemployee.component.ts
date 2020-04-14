6c71c1415cc465a414cebc5c6a63c308561c08de
rajeshvarikuntla <raj333921@gmail.com>
2020-04-15 04:24:56 +0530

    Added date fields and able to upload image in WP module

--- src/main/js/eweb/src/app/ehome/addemployee/addemployee.component.ts
+++ src/main/js/eweb/src/app/ehome/addemployee/addemployee.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {AddEmpDet} from './addEmpDet';
-import {AddEmpRes} from './addEmpRes';
 import {EmployeeDetailsRequestDto} from './employeeDetailsRequestDto'
 import {EmployeePassportDetailRequestDto} from './employeePassportDetailRequestDto';
 import {EmployeeFamilyDetailsRequestDto} from './employeeFamilyDetailsRequestDto';
@@ -25,9 +24,11 @@ empDataPass: EmployeePassportDetailRequestDto[] = [];
 empDataWpd: EmployeeWorkPermitDetailsRequestDto[] = [];
 empDataFd: EmployeeFamilyDetailsRequestDto[] = [];
 submitted = false;
+imageError = "";
 error = false;
 success = false;
 id = "0";
+files: any[];
 addEmpRes: EmployeeDetailsRequestDto = new EmployeeDetailsRequestDto();
 constructor(private ewebService: EwebcallService,private router: Router,private storageService: StorageService) { }
 
@@ -40,17 +41,23 @@ constructor(private ewebService: EwebcallService,private router: Router,private 
       this.empDet.adminuserName = this.ehomeDetails.userName;
       this.empDet.companyId = this.ehomeDetails.companyId;
       console.log(this.empPass);
-      if(this.empPass != null){
+      if(this.empPass != null && JSON.stringify(this.empPass) != "{}"){
+         if (this.empDataPass.indexOf(this.empPass) === -1){
           this.empDataPass.push(this.empPass);
           this.empDet.employeeDetailsRequestDto.employeePassportDetailRequestDto = this.empDataPass;
+        }
       }
-      if(this.empWpd != null){
+      if(this.empWpd != null && JSON.stringify(this.empWpd) != "{}"){
+          if (this.empDataWpd.indexOf(this.empWpd) === -1){
           this.empDataWpd.push(this.empWpd);
           this.empDet.employeeDetailsRequestDto.employeeWorkPermitDetailsRequestDto = this.empDataWpd;
+        }
       }
-      if(this.empFd != null){
+      if(this.empFd != null && JSON.stringify(this.empFd) != "{}"){
+         if (this.empDataFd.indexOf(this.empFd) === -1){
           this.empDataFd.push(this.empFd);
           this.empDet.employeeDetailsRequestDto.employeeFamilyDetailsRequestDto = this.empDataFd;
+        }
       }
       if(this.id != "0"){
           this.empDet.employeeDetailsRequestDto.id= this.id;
@@ -83,5 +90,33 @@ constructor(private ewebService: EwebcallService,private router: Router,private 
       this.saveMe();
     }
 
+  getBase64(file) {
+   var reader = new FileReader();
+   reader.readAsDataURL(file);
+   reader.onload = (e) => {
+      this.empWpd.documentData = reader.result;
+     //console.log(reader.result);
+   };
+   reader.onerror = function (error) {
+     console.log('Error: ', error);
+   };
+}
+
+
+  onFileChange(event){
+    let af = ['image/png', 'image/jpeg','application/pdf']
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      // console.log(file);
+     /* if (!_.includes(af, file.type)) {
+        alert('Only PNG/JPEG/PDF formats Allowed!');
+        this.imageError = 'Only Images are allowed ( JPG | PNG | PDF )';
+        return false
+      }*/
+
+        this.empWpd.documentName = file.name;
+        this.getBase64(file);
+      }
+    }
 
 }
