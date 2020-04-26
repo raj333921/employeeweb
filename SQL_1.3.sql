046f5a16c314290dc27ac0cb9432650f4734c00b
rajeshvarikuntla <raj333921@gmail.com>
2020-04-27 02:01:03 +0530

    latest as 1.4 sql with upload and download

--- SQL_1.3.sql
+++ SQL_1.3.sql
@@ -1,3 +1,4 @@
+DROP DATABASE employeeproduct;
 CREATE DATABASE employeeproduct;
 CREATE TABLE employeeproduct.users (
   `user_name` varchar(255) PRIMARY KEY,
@@ -44,18 +45,20 @@ CREATE TABLE employeeproduct.workpermit_details (
 CREATE TABLE employeeproduct.workpermit_document_details (
   `workpermit_number` varchar(255) PRIMARY KEY,
   `document_name` varchar(255),
-  `document_data` varchar(255)
+  `document_data` longblob
 );
 
+
 CREATE TABLE employeeproduct.payslip_details (
-  `payslip_number` int PRIMARY KEY AUTO_INCREMENT,
+  `payslip_number` varchar(255) PRIMARY KEY,
   `employee_id` int,
   `payslip_month` varchar(255)
 );
 
 CREATE TABLE employeeproduct.payslip_document_details (
-  `payslip_id` int PRIMARY KEY,
-  `document_data` varchar(255)
+  `payslip_number` varchar(255) PRIMARY KEY,
+  `document_name` varchar(255),
+  `document_data` longblob
 );
 
 CREATE TABLE employeeproduct.passport_details (
@@ -75,6 +78,13 @@ CREATE TABLE employeeproduct.family_details (
   `relation` int,
   `contact_number` varchar(255)
 );
+
+CREATE TABLE employeeproduct.passport_document_details (
+  `passport_number` varchar(255) PRIMARY KEY,
+  `document_name` varchar(255),
+  `document_data` longblob
+);
+
 
 CREATE TABLE employeeproduct.company (
    `id` int PRIMARY KEY AUTO_INCREMENT,
@@ -115,44 +125,12 @@ ALTER TABLE employeeproduct.workpermit_document_details ADD FOREIGN KEY (`workpe
 
 ALTER TABLE employeeproduct.payslip_details ADD FOREIGN KEY (`employee_id`) REFERENCES employeeproduct.employee (`id`);
 
-ALTER TABLE employeeproduct.payslip_document_details ADD FOREIGN KEY (`payslip_id`) REFERENCES employeeproduct.payslip_details (`payslip_number`);
+ALTER TABLE employeeproduct.payslip_document_details ADD FOREIGN KEY (`payslip_number`) REFERENCES employeeproduct.payslip_details (`payslip_number`);
 
 ALTER TABLE employeeproduct.passport_details ADD FOREIGN KEY (`employee_id`) REFERENCES employeeproduct.employee (`id`);
 
 ALTER TABLE employeeproduct.family_details ADD FOREIGN KEY (`employee_id`) REFERENCES employeeproduct.employee (`id`);
 
--- Company Table : 
-INSERT INTO employeeproduct.company (`id`, `name`, `email_id`, `address1`, `address2`, `city`, `state`, `country`, `contact_number`, `size`, `active`) VALUES ('2', 'aksdfjlasdk', 'adi.1293@gmail.com', 'Mahalakshmi Nagar', '6th cross street', 'chennai', 'tamilNadu', 'India', '9410', '0', '0');
-
--- Users Table : 
-INSERT INTO employeeproduct.users (`user_name`, `first_name`, `last_name`, `password`, `role`, `country`, `company_id`, `active`) VALUES ('adi.1293@gmail.com', 'Balachandar', 'Gopalan', 'Shyamgops@1', '0', 'India', '2', '0');
-
-INSERT INTO employeeproduct.employee (`id`, `address1`, `address2`, `city`, `contact_number`, `country`, `date_of_birth`, `department`, `email_id`, `first_name`, `job_role`, `last_name`, `repoting_person`, `sex`, `state`, `work_location`, `company_id`) VALUES (2, 'dasdfasd', 'dadafads', 'asdfasdf', 'adfdasf', 'asdfsadf', '1990-08-30', 'asdfsaf', 'asdfadsf', 'adfadsf', 'adfasdf', 'asdfadsf', '1', 'adf', 'adfasdf', 'adfasdf', '2');
-
--- WorkPermitDetails Table :
-INSERT INTO employeeproduct.workpermit_details (`employee_id`, `workpermit_number`, `start_date`, `end_date`, `validity`) VALUES (2, 3333333, '2020-01-08', '2023-01-07', 1);
-
-INSERT INTO employeeproduct.workpermit_details (`employee_id`, `workpermit_number`, `start_date`, `end_date`, `validity`) VALUES (2, 3333333333, '2018-01-08', '2020-01-07', 0);
-
-
---  passport_Details Table
-INSERT INTO employeeproduct.passport_details (`employee_id`, `passport_number`, `start_date`, `end_date`, `issue_place`, `validity`) VALUES (2, 'fgreere', '2011-08-30', '2021-07-31', 'Chennai', '1');
-
-INSERT INTO employeeproduct.passport_details (`employee_id`, `passport_number`, `start_date`, `end_date`, `issue_place`, `validity`) VALUES (2, 'werereerreewrerwerw', '2001-08-31', '2011-07-31', 'Chennai', '0');
-
---  workpermit_document_details
-INSERT INTO employeeproduct.workpermit_document_details (`workpermit_number`, `document_name`, `document_data`) VALUES (3333333, 'sadfdfdf', 'sdfsadfdsaffas');
-
-INSERT INTO employeeproduct.workpermit_document_details (`workpermit_number`, `document_name`, `document_data`) VALUES (3333333333, 'sadfdfdf', 'sdfsadfdsaffas');
-
-
-INSERT INTO employeeproduct.payslip_details (`payslip_number`, `payslip_month`, `employee_id`) VALUES (2, 'sdafsadf', 2);
-
---  payslip_document_details
-INSERT INTO employeeproduct.payslip_document_details (`payslip_id`, `document_data`) VALUES (2, 'adfasdf');
-
---  family_details
-INSERT INTO employeeproduct.family_details (`employee_id`,`first_name`, `last_name`, `relation`,`contact_number`) VALUES (2, 'asjdfkjds', 'adfasdfds', '0','9884941049');
-
+ALTER TABLE employeeproduct.passport_document_details ADD FOREIGN KEY (`passport_number`) REFERENCES employeeproduct.passport_details (`passport_number`);
 
 
