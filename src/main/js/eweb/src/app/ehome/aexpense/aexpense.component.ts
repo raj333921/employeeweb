aa5e67ac489d98ade9f8f91429db2a91576d51fc
rajeshvarikuntla <raj333921@gmail.com>
2020-05-16 02:46:16 +0530

    Expense Module

---
+++ src/main/js/eweb/src/app/ehome/aexpense/aexpense.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aexpense',
+  templateUrl: './aexpense.component.html',
+  styleUrls: ['./aexpense.component.css']
+})
+export class AexpenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/main/js/eweb/src/app/ehome/empexpense/empexpense.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empexpense',
+  templateUrl: './empexpense.component.html',
+  styleUrls: ['./empexpense.component.css']
+})
+export class EmpexpenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
